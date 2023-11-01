83ac9e932630a5ce9a3a13303731201e5c18a96d
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-11-01 10:59:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateTime() {
+    const timerElement = document.getElementById('timer');
+    const now = new Date();
+    
+    const options = { month: 'numeric', day: 'numeric',year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    const formattedTime = now.toLocaleString(undefined, options);
+    
+    timerElement.textContent = formattedTime;
+}
+
+setInterval(updateTime, 1000);
+
+updateTime();
